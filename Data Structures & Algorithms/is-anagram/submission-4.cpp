3f03e86994fc1bfda4b5f35ca236953741464313
Sandina <sandinacharles1@gmail.com>
2026-09-02 17:58:32 -0400

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        //1. If they're diff lengths they cant be the same
+        if (s.length() != t.length()){return false;}
+        //2. Create hashmap for char to frequencies so we can reference them
+        unordered_map<char, int> Char_To_Freq_S;
+        unordered_map<char, int> Char_To_Freq_T;
+        //3. Loop through all the words and update thei respective frequency charts. the [] automatically defaults to 0
+        for (int i = 0; i < s.length(); i++){
+            Char_To_Freq_S[s[i]] += 1;
+            Char_To_Freq_T[t[i]] += 1;
+        }  
+
+        return Char_To_Freq_S == Char_To_Freq_T;
+        }
+};
