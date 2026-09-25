e7936afa07c10c9d5faf1f7becf2c2389742c536
Sandina Charles <sandinacharles1@gmail.com>
2026-09-25 18:23:06 -0400

    Add: binary-tree-preorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-1.py
@@ -0,0 +1,54 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        '''
+        Depth-First search Logic :
+        For each parent, we check the left-most value down. Then go up and check right,
+        move up levels
+
+        Pre-Order:
+        This means we check left-most child first. We cann do this by checking the value, then
+        the left child, then thhe right
+
+        How to implement:
+        METHOD 1: Recursion:
+        We keep a base case of if the root is null so we stop that one recursive iteration
+        but otherwise we run the function on the function's children. First the left, it will
+        runn all its lefties first before left reaches null and we move onto the right. 
+        We make sure to include the work of appending to the output 
+
+        METHOD 2: ITERATIVE:
+        We maintain a stack. Its LIFO structure is helpful because we need to get deep into 
+        the most recently checked node. VISUALIZE THIS: We start the stack at the root. We
+        pop the root then append the right -> left. The left is the most recent in the stack,
+        now we pop the left and add its children right-> left. We stop and pop once left 
+        reaches null. Now, we go to the most recent right. Then the most recent right up
+
+        Edge case: Empty tree
+        '''
+        #Edge case
+        if root is None:
+            return []
+        stack = [root]
+        result = []
+
+        while stack:
+            #Add our current value onto the results list
+            current = stack.pop()
+            result.append(current.val)
+
+            if current.right is not None:
+                stack.append(current.right)
+            if current.left is not None:
+                stack.append(current.left)
+        
+        return result
+
+
+
+
